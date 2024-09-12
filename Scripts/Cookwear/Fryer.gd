9786a2f1fe69bfd854bcd525de9f68363be1c075
Martin Staab <martin.e.staab@gmail.com>
2024-09-12 16:41:24 -0700

    Renamed: scripts to fit with naming conventions

--- Scripts/Cookwear/Fryer.gd
+++ Scripts/Cookwear/Fryer.gd
@@ -1,5 +1,6 @@
 extends StaticBody3D
 
+
 @export var lerp_speed : float
 @export var max_height : float
 @export var min_height : float
@@ -20,10 +21,12 @@ func _process(delta: float) -> void:
 		handle_release(delta)
 		pass
 
+
 func on_interact() -> void:
 	previous_mouse_position = get_viewport().get_mouse_position()
 	clicked = true
-	
+
+
 func on_stop_interact() -> void:
 	clicked = false
 
@@ -37,12 +40,11 @@ func handle_holding(delta: float) -> void:
 	if max_height < position.y and delta_y > 0:
 		delta_y = 0
 	
-	
 	position.y = lerp(position.y, position.y + delta_y, lerp_speed * sensitivity * delta)
-		
 	
 	previous_mouse_position = mouse_position
-	
+
+
 func handle_release(delta: float) -> void:
 	if (position.y - min_height) > (max_height - position.y):
 		position.y = lerp(position.y, max_height, lerp_speed * delta)
