a331efaf8562d3c6341f2c13069c962bd9680db7
Inocybe <levmitchell@icloud.com>
2024-09-15 13:14:35 -0700

    basic scene setup for orders

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -18,9 +18,5 @@ func instantate_player() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_released("click"):
-		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
 	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		get_tree().quit()
