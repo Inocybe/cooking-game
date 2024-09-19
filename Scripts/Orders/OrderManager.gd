5f7f33b9475f762f29006b3e178d9ef65dae6a83
Lev Mitchell <levmitchell@icloud.com>
2024-09-19 13:49:48 -0700

    yuh

--- Scripts/Orders/OrderManager.gd
+++ Scripts/Orders/OrderManager.gd
@@ -2,8 +2,12 @@ extends Node
 
 const DISH = preload("res://Scenes/orders/dish.tscn")
 
-var dishes_in_scene: Array[Node3D]
 var order: OrderFunctions = null
+
+var dishes_in_scene: Array[Node3D]
+var order_array: Array[OrderFunctions]
+
+
 
 
 func _process(delta: float) -> void:
@@ -13,3 +17,9 @@ func new_order() -> void:
 	order = OrderFunctions.new()
 	order.create_random_order()
 	order.print_order_items()
+	order_array.append(order)
+
+func clear_orders() -> void:
+	order_array.clear()
+	for i in range(dishes_in_scene.size()):
+		dishes_in_scene.remove_at(i)
