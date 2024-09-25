fe50b181ceb4f5cc5b36fa6459662b3ce6879e77
Lev Mitchell <levmitchell@icloud.com>
2024-09-25 12:58:00 -0700

    ok

--- Scripts/Rigidbodies/RigidBodyBase.gd
+++ Scripts/Rigidbodies/RigidBodyBase.gd
@@ -1,7 +1,7 @@
 class_name RigidBodyBase extends RigidBody3D
 
-const LINEAR_THRESHOLD: float = 0.01
-const ANGULAR_THRESHOLD: float = 0.01
+
+const CONTINUOUS_CD_SPEED_CUTOFF: float = 0.5
 
 @export var max_speed: float = 5
 
@@ -9,3 +9,5 @@ const ANGULAR_THRESHOLD: float = 0.01
 func _physics_process(_delta: float) -> void:
 	if linear_velocity.length_squared() > max_speed * max_speed:
 		linear_velocity = linear_velocity.normalized() * max_speed
+		
+		continuous_cd = linear_velocity.length_squared() > CONTINUOUS_CD_SPEED_CUTOFF ** 2
