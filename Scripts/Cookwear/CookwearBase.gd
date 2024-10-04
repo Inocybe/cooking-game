8f6854145c2f2baaaac2203c57bc109e84a64fbb
Inocybe <140019717+Inocybe@users.noreply.github.com>
2024-10-04 11:19:36 -0700

    started work on making system better

---
+++ Scripts/Cookwear/CookwearBase.gd
@@ -0,0 +1,18 @@
+class_name CookwearBase extends StaticBody3D
+
+
+@export var cooking_area: Area3D = null
+
+func _ready() -> void:
+	cooking_area.body_entered.connect(on_body_entered)
+	cooking_area.body_exited.connect(on_body_exited)
+
+
+func on_body_entered(body: Node3D):
+	if body.has_method("on_start_cooking"):
+		body.on_start_cooking()
+
+
+func on_body_exited(body: Node3D):
+	if body.has_method("on_stop_cooking"):
+		body.on_stop_cooking()
