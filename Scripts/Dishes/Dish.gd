345ee25406418f3dde77a4ad75785a7990627b89
Lev Mitchell <levmitchell@icloud.com>
2024-10-17 13:00:40 -0700

    sad

--- Scripts/Dishes/Dish.gd
+++ Scripts/Dishes/Dish.gd
@@ -9,7 +9,8 @@ const force_amount: float = 0.005
 var order: Array[Menu.Item]
 var order_functions : OrderFunctions
 # tracking of food on tray
-var childed_objects: Array[Array]
+var childed_objects: Array[Array] = [[], [], []]
+var childed_ghosts: Array[Array] = [[], [], []]
 var recently_removed_child: Array[Holdable]
 
 @export var food_positions: Array[Node3D]
@@ -25,12 +26,13 @@ func _ready() -> void:
 		
 		var scene_counter = 0
 		
-		childed_objects.append([])
-		
 		# instantiates each item in the order thing yes (good comment ain't it)
 		for scene in order_functions.get_food_item_scenes(order[i]):
 			#instnatiate and add to childed objects
 			var food: Node3D = instantiate_scene_from_path(scene)
+			
+			# add to childed objects array
+			childed_ghosts[i].append(food)
 			
 			# combine object to the thing
 			combine_objects(food, i)
@@ -38,8 +40,7 @@ func _ready() -> void:
 			# add the ghost item
 			add_ghost_order_controller(food)
 			
-			# add to childed objects array
-			childed_objects[i].append(food)
+
 			
 			scene_counter += 1
 
@@ -92,19 +93,19 @@ func add_ghost_order_controller(object: Node3D) -> void:
 
 # detecting of a food objet goes near tray
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("food"):
+	if body.is_in_group("food") and not childed_ghosts.has(body):
 		if body.has_method("get_food_type") and !recently_removed_child.has(body):
 			check_order_and_add_object(body)
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	if body in recently_removed_child:
+	if body in recently_removed_child and not childed_ghosts.has(body):
 		recently_removed_child.erase(body)
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("remove_children") and childed_objects.size() > 0:
-		#remove_all_objects()
+		remove_all_objects()
 		pass
 
 
@@ -112,15 +113,17 @@ func check_order_and_add_object(body: Node3D) -> void:
 	for i in range(order.size()):
 		if body.get_food_type() == order[i]:
 			combine_objects(body, i)
+			childed_objects[i].append(body)
 			disable_ghost_object(i)
 
 
 func disable_ghost_object(index: int) -> void:
-	for item: Holdable in childed_objects[index]:
+	for item: Holdable in childed_ghosts[index]:
 		item.visible = false
 
+
 func remove_all_objects() -> void:
-	for i in range(order.size()):
+	for i in range(childed_objects.size()):
 		for child in childed_objects[i]:
 			var collider = child.get_node_or_null("CollisionShape3D")
 			
@@ -137,8 +140,15 @@ func remove_all_objects() -> void:
 			child.collision_layer = 1
 			child.collision_mask = 1
 			
+	
+	set_childed_objects_to_null()
 		
-	childed_objects.clear()
 	
 func object_removed(object: Node3D) -> void:
 	recently_removed_child.append(object)
+	
+	
+func set_childed_objects_to_null() -> void:
+	for i: int in range(childed_objects.size()):
+		childed_objects[i].clear()
+			
