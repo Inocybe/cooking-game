0edb3591d1d9c22a88342a233281a47a71f001cd
Lev Mitchell <levmitchell@icloud.com>
2024-10-29 13:51:18 -0700

    display works somewhat

--- Scripts/Dishes/OrderDisplay.gd
+++ Scripts/Dishes/OrderDisplay.gd
@@ -1,6 +1,15 @@
 extends Node3D
 
+
+# MAKE IN SAME ORDER AS MENU ITEMS
+@export var food_sprites: Array[CompressedTexture2D]
+@export var positions: Array[Sprite3D]
+
+
+
 var player: CharacterBody3D = null
+
+
 
 func _ready() -> void:
 	player = Global.game_manager.player
@@ -8,3 +17,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	look_at(player.global_transform.origin, Vector3(0, 1, 0))
+
+
+func set_order_display(items: Array[Menu.Item]) -> void:
+	for i: int in range(items.size()):
+		positions[i].texture = food_sprites[items[i]]
