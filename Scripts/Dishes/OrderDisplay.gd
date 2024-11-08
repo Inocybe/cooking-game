5d563b8b9163dc1f995397853e7ca7f8c9faca59
Martin Staab <martin.e.staab@gmail.com>
2024-11-08 14:19:37 -0800

    Removed: old food truck model

--- Scripts/Dishes/OrderDisplay.gd
+++ Scripts/Dishes/OrderDisplay.gd
@@ -1,10 +1,6 @@
 extends Node3D
 
-var player: CharacterBody3D = null
-
-func _ready() -> void:
-	player = Global.game_manager.player
-
 
 func _process(delta: float) -> void:
+	var player: CharacterBody3D = Global.game_manager.player
 	look_at(player.global_transform.origin, Vector3(0, 1, 0))
