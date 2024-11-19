c76e9372baa7ccb4c8901371ef167001628a19b8
Lev Mitchell <levmitchell@icloud.com>
2024-11-19 13:12:41 -0800

    can add orders, and when finished order and click bell removes the order

--- Scripts/Misc/finished_order_bell.gd
+++ Scripts/Misc/finished_order_bell.gd
@@ -1,15 +1,6 @@
 extends Node3D
 
 
-var game_manager: GameManager = null
-var food_truck: Node3D = null
-
-
-func _read() -> void:
-	game_manager = Global.game_manager
-	if game_manager:
-		food_truck = game_manager.food_truck
-
 
 func on_start_interact() -> void:
-	pass
+	Global.game_manager.food_truck.remove_completed_orders()
--- Scripts/Misc/food_truck.gd
+++ Scripts/Misc/food_truck.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 @export var dish_positions: Array[Node3D]
 var order_manager: OrderManager
+var orders: Array[Node3D]
 
 
 
@@ -14,12 +15,24 @@ func add_order() -> void:
 	for pos in dish_positions:
 		if pos.get_child_count() == 0:
 			var order: Node3D = order_manager.new_order()
+			orders.append(order)
 			order.reparent(pos)
 			order.global_position = pos.global_position
 			order.global_rotation = pos.global_rotation
 			
 			return
 
-#func _input(event: InputEvent) -> void:
-#	if Input.is_key_pressed(KEY_G):
-#		add_order()
+
+func remove_completed_orders() -> void:
+	for order in orders:
+		
+		if order.has_method("is_order_complete"):
+			if order.is_order_complete():
+				print("made it here")
+				orders.erase(order)
+				order.free()
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_G):
+		add_order()
