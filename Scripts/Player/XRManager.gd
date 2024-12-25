1005fa367c9be46831677e1d125ebcc9fc7d77e7
Martin Staab <martin.e.staab@gmail.com>
2024-12-24 19:21:49 -0800

    Changed: misc things to make VR work better

--- Scripts/Player/XRManager.gd
+++ Scripts/Player/XRManager.gd
@@ -5,12 +5,12 @@ var xr_interface: XRInterface
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
-		print("OpenXR initialized successfully")
+		print("OpenXR winitialized successfully")
 
 		# Turn off v-sync!
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
-		# Change our main viewport to output to the HMD
+		# Change our main viewport toaw output to the HMD
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
