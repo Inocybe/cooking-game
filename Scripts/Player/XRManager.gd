f36a5471186e2c75568408fde84051b33c88273a
budopod1 <martin.e.staab@gmail.com>
2025-01-15 13:02:29 -0800

    Added: basic movement in VR

--- Scripts/Player/XRManager.gd
+++ Scripts/Player/XRManager.gd
@@ -1,21 +1,15 @@
-extends Node
+class_name XRManager extends Node
+
+
+@onready var left_hand: XRController3D = $"Left Hand"
+@onready var right_hand: XRController3D = $"Right Hand"
 
 var xr_interface: XRInterface
-var initilized: bool = false
+
 
 func _ready():
-	xr_interface = XRServer.find_interface("OpenXR")
-	if xr_interface and xr_interface.is_initialized():
-		print("OpenXR winitialized successfully")
-		
-		# Turn off v-sync!
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-		
-		# Change our main viewport to output to the HMD
-		get_viewport().use_xr = true
-		initilized = true
-	else:
-		print("OpenXR not initialized, please check if your headset is connected")
-
+	# Turn off v-sync!
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 	
-	#get_parent().open_xr_initialized(initilized, self)
+	# Change our main viewport to output to the HMD
+	get_viewport().use_xr = true
