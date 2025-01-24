084d49ea5b102adb08d3be9dedc729e419a51016
budopod1 <martin.e.staab@gmail.com>
2025-01-24 11:25:51 -0800

    Changed: UI to use collumns

---
+++ Scripts/UI/Maximizer.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export var relative_to: Control
+
+@export var apply_to_x: bool = false
+@export var apply_to_y: bool = false
+@export var x_padding: float = 0
+@export var y_padding: float = 0
+
+
+func _ready() -> void:
+	handle_resize()
+	get_parent().resized.connect(handle_resize)
+
+
+func handle_resize() -> void:
+	var parent_size: Vector2 = relative_to.size
+	if apply_to_x:
+		size.x = parent_size.x - x_padding * 2
+	if apply_to_y:
+		size.y = parent_size.y - y_padding * 2
