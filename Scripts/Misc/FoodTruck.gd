642978958e0da03e31510c186c41d066cccc6438
budopod1 <martin.e.staab@gmail.com>
2025-02-05 12:55:28 -0800

    Fixed: food trays stacking unnecessarily

--- Scripts/Misc/FoodTruck.gd
+++ Scripts/Misc/FoodTruck.gd
@@ -2,6 +2,7 @@ class_name FoodTruck extends Node3D
 
 
 @export var dish_spawnpoints: Array[Node3D] = []
+@export var dish_check_height: float = 1
 
 @onready var ordering_position: Node3D = $"OrderingPosition"
 @onready var finished_order_position: Area3D = $Areas/FinishedOrderPosition
@@ -13,8 +14,8 @@ func _ready() -> void:
 
 
 func is_dish_position_occupied(pos: Node3D) -> bool:
-	var from: Vector3 = pos.global_position
-	var to: Vector3 = from + Vector3.DOWN
+	var from: Vector3 = pos.global_position + Vector3.UP * dish_check_height
+	var to: Vector3 = from + Vector3.DOWN * (dish_check_height * 2)
 	var space = get_world_3d().direct_space_state
 	var ray_query = PhysicsRayQueryParameters3D.create(from, to)
 	var raycast = space.intersect_ray(ray_query)
