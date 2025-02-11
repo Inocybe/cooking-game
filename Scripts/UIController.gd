a37e52de19edc5a118dc08851477aa6cafc88524
budopod1 <martin.e.staab@gmail.com>
2025-02-11 13:42:58 -0800

    Fixed: UI code

--- Scripts/UIController.gd
+++ Scripts/UIController.gd
@@ -1,14 +1,12 @@
 extends Node3D
 
-@onready var money: Label = $Sprite3D/SubViewportContainer/SubViewport/Control/HBoxContainer/Middle/VBoxContainer/Money
-@onready var orders_complete: Label = $Sprite3D/SubViewportContainer/SubViewport/Control/HBoxContainer/Middle/VBoxContainer/OrdersComplete
+
+@onready var main_control = %MainControl
 
 
 func change_money(current_money: float) -> void:
-	if money:
-		money.set_text(str(current_money))
+	main_control.money.set_text(str(current_money))
 
 
 func change_order_complete_count(current_orders_complete: int) -> void:
-	if orders_complete:
-		orders_complete.set_text(str(current_orders_complete))
+	main_control.orders_complete.set_text(str(current_orders_complete))
