ca0fc06bc57d6a849cbcf909e035b5958019cc44
Inocybe <140019717+Inocybe@users.noreply.github.com>
2025-02-24 13:34:21 -0800

    working cooking particels for burger

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,5 +1,13 @@
 extends Node3D
 
 
+@onready var game_manager: GameManager = $GameManager
+@onready var xrui: Node3D = $XRUI
+@onready var desktop_ui: Node3D = $DesktopUI
+
 func _ready() -> void:
-	
+	game_manager.connect("XR_Detected", on_xr_detection)
+
+func on_xr_detection() -> void:
+	xrui.visible = true
+	desktop_ui.visible = false
