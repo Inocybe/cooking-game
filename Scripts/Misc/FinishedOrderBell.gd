4045ff0b31c9db19ae47b3185fb8e1dac56b4171
Martin Staab <martin.e.staab@gmail.com>
2025-03-12 19:35:16 -0700

    Added: real bell model

--- Scripts/Misc/FinishedOrderBell.gd
+++ Scripts/Misc/FinishedOrderBell.gd
@@ -2,5 +2,7 @@ extends Node3D
 
 
 func on_start_interact() -> void:
-	Global.order_manager.call_customers_with_completed_orders()
-	$BellSoundPlayer.play()
+	if not $BellSwingAnimation.is_playing():
+		Global.order_manager.call_customers_with_completed_orders()
+		$BellSoundPlayer.play()
+		$BellSwingAnimation.play("swing")
