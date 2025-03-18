a0e15b972819c17babf041e6f7590489f76a5774
budopod1 <martin.e.staab@gmail.com>
2025-03-18 16:30:50 -0700

    Added: sound when food is placed in trash

--- Scripts/Foodtruck/Interior/Cookwear/Trashcan/TrashCan.gd
+++ Scripts/Foodtruck/Interior/Cookwear/Trashcan/TrashCan.gd
@@ -8,3 +8,5 @@ func _ready() -> void:
 func body_entered_can(body: Node3D) -> void:
 	if body.is_in_group("holdable") and not body.freeze:
 		body.get_parent().remove_child(body)
+		if not $TrashSound.playing:
+			$TrashSound.play()
