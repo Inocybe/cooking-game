dc2fc1c784da04882a6a10235861c90ee75f0ead
Inocybe <140019717+Inocybe@users.noreply.github.com>
2025-03-18 13:50:19 -0700

    made new ui object's controls and started fixing interactions

---
+++ Scripts/UI/Map/food_graph.gd
@@ -0,0 +1,20 @@
+extends TextureRect
+
+@onready var chart: ColorRect = $Chart
+@onready var label: Label = $FoodLable/Label
+
+const CHART_SCALE_MULTIPLIER: int = 25
+
+
+func set_food_label(name: String) -> void:
+	label.text = name
+
+func set_chart_length_and_text(amount: int) -> void:
+	set_chart_label_text(amount)
+	set_chart_length(amount)
+
+func set_chart_label_text(amount: int) -> void:
+	chart.get_child(0).text = amount
+
+func set_chart_length(amount: int) -> void:
+	chart.custom_minimum_size.x = amount * CHART_SCALE_MULTIPLIER
