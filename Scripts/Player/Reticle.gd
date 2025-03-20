5bf3c2d0b878f554606f1f9af0defe75ba152ec7
budopod1 <martin.e.staab@gmail.com>
2025-03-20 13:34:25 -0700

    Changed: made reticle size increase with screen size

--- Scripts/Player/Reticle.gd
+++ Scripts/Player/Reticle.gd
@@ -1,7 +1,8 @@
 extends CenterContainer
 
 
-@export var dot_radius: float = 1.0
+@export var dot_min_radius: float = 1.0
+@export var dot_vw_radius: float = 1.0
 @export var dot_color: Color = Color.WHITE
 
 
@@ -10,4 +11,6 @@ func _ready() -> void:
 
 
 func _draw() -> void:
-	draw_circle(Vector2(0, 0), dot_radius, dot_color)
+	var vw: float = get_viewport().size.x / 100.0
+	var radius: float = max(dot_min_radius, vw * dot_vw_radius)
+	draw_circle(Vector2(0, 0), radius, dot_color)
