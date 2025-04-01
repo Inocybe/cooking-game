6a5e24480bcbf0fcf295cba63173ee7928ee0a18
budopod1 <martin.e.staab@gmail.com>
2025-04-01 13:46:41 -0700

    Added: mop cleaning sound effect

--- Scenes/player/tools/MopBrush.gd
+++ Scenes/player/tools/MopBrush.gd
@@ -3,16 +3,22 @@ extends Node3D
 
 @export var cleaning_effectiveness: float = 0.3
 
+@export var swish_sound_speed: float = 0.5
+
 var last_position: Vector3
 
 var in_spills: Array[Spill] = []
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	in_spills = in_spills.filter(is_instance_valid)
 	
 	var movement = (global_position - last_position).length()
 	last_position = global_position
+	
+	var speed = movement / delta
+	if in_spills.size() > 0 and speed > swish_sound_speed:
+		play_swish_sound()
 	
 	for spill: Spill in in_spills:
 		spill.reduce_size(cleaning_effectiveness * movement)
@@ -20,6 +26,11 @@ func _process(_delta: float) -> void:
 	var pole_facing = %PoleMesh.global_basis.y
 	var yrot = -atan2(pole_facing.z, pole_facing.x)
 	global_rotation = Vector3(0, yrot, 0)
+
+
+func play_swish_sound() -> void:
+	if not $SwishSoundPlayer.playing:
+		$SwishSoundPlayer.play()
 
 
 func on_area_entered(area: Area3D):
