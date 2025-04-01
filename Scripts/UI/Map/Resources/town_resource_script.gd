da902fb186890845de33389c04f18bc88960d00a
Inocybe <140019717+Inocybe@users.noreply.github.com>
2025-04-01 13:51:24 -0700

    started work on town and finished interactions

--- Scripts/UI/Map/Resources/town_resource_script.gd
+++ Scripts/UI/Map/Resources/town_resource_script.gd
@@ -1,15 +1,15 @@
 class_name town extends Resource
 
-enum weather_types {
-	overcast, 
-	sunny,
-	runny,
-	storm,
-	unknown
+var weather_types: Dictionary[int, String] = {
+	0: "overcast",
+	1: "sunny",
+	2: "ranny",
+	3: "storm",
+	4: "unknown"
 }
 
 const temp_min: float = -10.0
-const temp_max: float = 34.0
+const temp_max: float = 46.0
 
 const pop_min: int = 50
 const pop_max: int = 500
@@ -17,7 +17,7 @@ const pop_max: int = 500
 
 @export_range(temp_min, temp_max, 0.5) var tempurature: float
 @export_range(pop_min, pop_max, 1) var population: int
-@export var weather: weather_types
+@export var weather: String
 
 func random_everything() -> void:
 	random_tempurature()
@@ -25,11 +25,12 @@ func random_everything() -> void:
 	random_weather_off_temp()
 
 func random_tempurature() -> void:
-	tempurature = randf_range(temp_min, temp_max)
+	tempurature = snappedf(randf_range(temp_min, temp_max), 0.01)
 
 func random_population() -> void:
 	population = randi_range(pop_min, pop_max)
 
 func random_weather_off_temp() -> void:
 	# NOT IMPLEMENTED OFF TEMP YET
-	weather = weather_types.values()[randi() %weather_types.size()]
+	var i: int = randi_range(0, weather_types.size() - 1) 
+	weather = weather_types[i]
