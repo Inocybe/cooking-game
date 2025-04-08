738d70ff1548b32ee3b5834c5a2804adb963ef9b
budopod1 <martin.e.staab@gmail.com>
2025-04-08 16:20:52 -0700

    Added: rain sound playing

--- Scripts/Enviornment/RainArea.gd
+++ Scripts/Enviornment/RainArea.gd
@@ -1,5 +1,26 @@
 class_name RainArea extends Node3D
 
 
+var heavy_raininess_cutoff: float = 0.75
+var medium_raininess_cutoff: float = 0.25
+
+
+func _ready() -> void:
+	set_raininess(1)
+
+
 func set_raininess(raininess: float) -> void:
 	$GPUParticles3D.amount_ratio = raininess
+	
+	var is_rain_heavy = raininess > heavy_raininess_cutoff
+	var is_rain_medium = raininess > medium_raininess_cutoff and not is_rain_heavy
+	
+	if is_rain_heavy:
+		$HeavyRainSoundFader.fade_in()
+	else:
+		$HeavyRainSoundFader.fade_out()
+	
+	if is_rain_medium:
+		$MediumRainSoundFader.fade_in()
+	else:
+		$MediumRainSoundFader.fade_out()
