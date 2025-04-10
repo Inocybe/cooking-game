4ecc43db9840184097562fba095c2e5bf5125f84
budopod1 <martin.e.staab@gmail.com>
2025-04-10 16:06:04 -0700

    Added: sun brightness changing with cloud cover, grouping of props in Cookable and Holdable

--- Scripts/Enviornment/SunManager.gd
+++
@@ -1,9 +0,0 @@
-extends DirectionalLight3D
-
-
-func _ready() -> void:
-	Global.game_manager.XR_detected.connect(disable_light)
-
-
-func disable_light() -> void:
-	sky_mode = DirectionalLight3D.SKY_MODE_SKY_ONLY
