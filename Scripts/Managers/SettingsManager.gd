eb694cf7a427631c8e687192a9f59fefb5e4ccee
Martin Staab <martin.e.staab@gmail.com>
2025-04-12 19:38:03 -0700

    Added: settings persistence

--- Scripts/Managers/SettingsManager.gd
+++ Scripts/Managers/SettingsManager.gd
@@ -1,18 +1,23 @@
 extends Node
 
 
+const SETTINGS_PATH = "user://settings.json"
+
 var overall_volume: float = 1
 var music_volume: float = 1
 
 
+signal settings_updated()
+
+
 func _ready() -> void:
 	settings_updated.connect(do_volume_updates)
-	# load settings here
+	load_settings()
 	settings_updated.emit()
 
 
 func substantiate_settings() -> void:
-	# save settings here
+	save_settings()
 	settings_updated.emit()
 
 
@@ -28,16 +33,26 @@ func do_volume_updates() -> void:
 	)
 
 
-func get_settings_json() -> Dictionary:
-	return {
+func save_settings() -> void:
+	var file = FileAccess.open(SETTINGS_PATH, FileAccess.WRITE)
+	file.store_string(get_settings_json_txt())
+
+
+func load_settings() -> void:
+	if not FileAccess.file_exists(SETTINGS_PATH):
+		return
+	var file = FileAccess.open(SETTINGS_PATH, FileAccess.READ)
+	read_settings_json_txt(file.get_as_text())
+
+
+func get_settings_json_txt() -> String:
+	return JSON.stringify({
 		"overall_volume": overall_volume,
 		"music_volume": music_volume
-	}
+	})
 
 
-func read_settings_json(json: Dictionary) -> void:
+func read_settings_json_txt(json_txt: String) -> void:
+	var json: Dictionary = JSON.parse_string(json_txt)
 	overall_volume = json["overall_volume"]
 	music_volume = json["music_volume"]
-
-
-signal settings_updated()
--- Scripts/UI/SettingControl.gd
+++ Scripts/UI/SettingControl.gd
@@ -2,14 +2,12 @@ extends Control
 
 
 @export var prop_name: String
-@export var signal_name: String
 @export var setting_name: String
 
 
 func _ready() -> void:
 	set(prop_name, SettingsManager.get(setting_name))
-	get(signal_name).connect(value_changed)
 
 
-func value_changed() -> void:
+func change_setting(v=null) -> void:
 	SettingsManager.set(setting_name, get(prop_name))
