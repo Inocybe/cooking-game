5c28f303bd7f5e00582d2bb6974a383662f69f52
budopod1 <martin.e.staab@gmail.com>
2025-04-15 13:05:41 -0700

    Added: icon | Fixed: UI not scaling with window

--- Scenes/ui/controls/VRSubViewportMover.gd
+++ Scenes/ui/controls/VRSubViewportMover.gd
@@ -8,3 +8,10 @@ func _ready() -> void:
 func on_has_XR_detected(has_XR: bool) -> void:
 	if has_XR:
 		reparent.call_deferred(get_parent().get_parent())
+	else:
+		get_tree().get_root().size_changed.connect(screen_resize)
+		screen_resize()
+
+
+func screen_resize() -> void:
+	size = get_parent().get_viewport().size
