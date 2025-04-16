e1f9a4e020578d574ec338650537c606f5905669
budopod1 <martin.e.staab@gmail.com>
2025-04-15 18:51:42 -0700

    Fixed: made MapButtons responsive to screen size

--- Scripts/UI/Map/MapButton.gd
+++ Scripts/UI/Map/MapButton.gd
@@ -1,38 +1,49 @@
-extends Button
+extends Control
 
 
 const SCENE_ATTACHED: String = "res://Scenes/mains/world.tscn"
 
 @onready var town: TownResource = TownResource.new()
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var label: TextEdit = $TextEdit
+@onready var label: RichTextLabel = $TextHolder/TextPanel/TextMargin/Text
 
-var displaying: bool = false
+@export var position_fraction: Vector2
+
+var town_values_shown: bool = false
 
 
-func start() -> void:
+func _ready() -> void:
+	$Button.pressed.connect(on_button_pressed)
+	
+	get_parent().resized.connect(reposition)
+	reposition()
+	
 	town.random_everything()
 	set_town_values()
 	async_load()
 
 
-func _on_pressed() -> void:
-	print("pressed")
-	if !displaying:
-		displaying = true
-		animation_player.play("display_town_values")
-	else:
-		displaying = false
+func reposition() -> void:
+	position = position_fraction * get_parent().size
+
+
+func on_button_pressed() -> void:
+	if town_values_shown:
+		town_values_shown = false
 		Global.town = town
 		Global.switch_scenes(ResourceLoader.load_threaded_get(SCENE_ATTACHED))
-
+	else:
+		town_values_shown = true
+		animation_player.play("display_town_values")
 
 
 func set_town_values() -> void:
 	var weather_name = WeatherManager.get_weather_name(town.weather)
-	label.set_line(0, "Weather: " + weather_name)
-	label.set_line(1, "Temperature: " + str(town.temperature) + "°C")
-	label.set_line(2, "Population: " + str(town.population))
+	label.text = """Weather: %s
+Temperature: %s°C
+Population: %s""" % [
+		weather_name, str(town.temperature), str(town.population)
+	]
 
 
 func async_load() -> void:
