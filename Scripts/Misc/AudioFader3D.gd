5779d629b0c59fa27ed762b1ac89bfd1896ef6b9
Martin Staab <martin.e.staab@gmail.com>
2025-04-16 17:22:08 -0700

    Fixed: rain sounds, frying looping | Added: truck chair collision

--- Scripts/Misc/AudioFader3D.gd
+++ Scripts/Misc/AudioFader3D.gd
@@ -4,16 +4,19 @@ class_name AudioFader3D extends AudioStreamPlayer3D
 @export var fade_in_time: float = 0.1
 @export var fade_out_time: float = 0.4
 
+var initiated: bool = false
 var is_audible: bool = false
 var full_volume: float
 
 
 func _ready() -> void:
-	full_volume = volume_linear
-	volume_linear = 0
+	if not initiated:
+		init()
 
 
 func fade_in() -> void:
+	if not initiated:
+		init()
 	if is_audible:
 		return
 	if not get_tree():
@@ -24,6 +27,8 @@ func fade_in() -> void:
 
 
 func fade_out() -> void:
+	if not initiated:
+		init()
 	if not is_audible:
 		return
 	if not get_tree():
@@ -31,3 +36,9 @@ func fade_out() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "volume_linear", 0, fade_out_time)
 	is_audible = false
+
+
+func init() -> void:
+	initiated = true
+	full_volume = volume_linear
+	volume_linear = 0
