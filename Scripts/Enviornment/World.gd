313900a6cb6d9c6194bfb0613e32efa99cec8258
Martin Staab <martin.e.staab@gmail.com>
2025-04-16 20:15:34 -0700

    Added: timer label | Simplified: town loading sequence

--- Scripts/Enviornment/World.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-
-func _ready() -> void:
-	if Global.town != null:
-		Global.game_manager.town_manager.load_town(Global.town.town_int)
--- Scripts/Managers/TownManager.gd
+++ Scripts/Managers/TownManager.gd
@@ -1,17 +1,15 @@
 class_name TownManager extends Node
 
 
-var towns: Dictionary[int, String] = {
+var town_scene_paths: Dictionary[int, String] = {
 	0: "res://Scenes/towns/town_1.tscn"
 }
 
 
-func _ready() -> void:
-	for i in towns.size():
-		ResourceLoader.load_threaded_request(towns[i])
-
-
-func load_town(town: int) -> void:
-	var town_scene: PackedScene = ResourceLoader.load_threaded_get(towns[town])
+func load_town(town: TownResource) -> void:
+	Global.game_manager.weather_manager.set_from_town(town)
+	var path = town_scene_paths[town.town_int]
+	ResourceLoader.load_threaded_request(path, "", true)
+	var town_scene: PackedScene = ResourceLoader.load_threaded_get(path)
 	var instance: Node3D = town_scene.instantiate()
-	get_tree().current_scene.add_child(instance)
+	get_tree().current_scene.add_child.call_deferred(instance)
