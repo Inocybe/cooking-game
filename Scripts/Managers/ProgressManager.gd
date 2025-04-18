4b7bc3b5e657916e2f16018ee574ea433b25dc94
Martin Staab <martin.e.staab@gmail.com>
2025-04-17 20:36:19 -0700

    Added: storage to ProgressManager

--- Scripts/Managers/ProgressManager.gd
+++ Scripts/Managers/ProgressManager.gd
@@ -1,4 +1,14 @@
 extends Node
 
 
-var stock: Array[Menu.FoodComponent] = []
+class StoredFoodComponent:
+	var food: Menu.FoodComponent
+	
+	func _init(food_: Menu.FoodComponent) -> void:
+		self.food = food_
+
+
+var stock: Array[StoredFoodComponent] = []
+var day: int = 1
+var orders_complete: int = 0
+var money: float = 0
