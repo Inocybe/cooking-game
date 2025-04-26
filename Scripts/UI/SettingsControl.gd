b5df77324058c91ee8e8ced58e225bc7ef46a350
Martin Staab <martin.e.staab@gmail.com>
2025-04-26 12:50:37 -0700

    Added: 'Open Progress Folder' button in settings

--- Scripts/UI/SettingsControl.gd
+++ Scripts/UI/SettingsControl.gd
@@ -4,12 +4,16 @@ extends Control
 func _ready() -> void:
 	var viewport = get_viewport()
 	if viewport is SubViewport and viewport.get_parent() is TV:
-		%ResetProgressRow.queue_free()
+		%ManageProgressRow.queue_free()
 
 
 func exit_settings() -> void:
 	SettingsManager.substantiate_settings()
 	visible = false
+
+
+func open_progress_location() -> void:
+	OS.shell_show_in_file_manager(ProjectSettings.globalize_path("user://"))
 
 
 func reset_progress() -> void:
