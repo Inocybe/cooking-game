4f73787e2d2c5c28472cd5884271fdfebbb38ef6
budopod1 <martin.e.staab@gmail.com>
2025-04-29 12:49:02 -0700

    Fixed: font size scaling in food truck

--- Scripts/UI/Utils/FontScaler.gd
+++ Scripts/UI/Utils/FontScaler.gd
@@ -9,12 +9,13 @@ var target: Control
 
 func _ready():
 	target = get_parent()
-	update_font_size()
+	update_font_size.call_deferred()
 	if not Engine.is_editor_hint():
 		Global.font_size_changed.connect(update_font_size)
 
 
 func update_font_size() -> void:
+	var base_font_size = target.get_theme_default_font_size()
 	target.add_theme_font_size_override(
-		"font_size", int(target.get_theme_default_font_size() * font_scale)
+		"font_size", int(base_font_size * font_scale)
 	)
