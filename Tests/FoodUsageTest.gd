91f720d883054adbb57f2a63af8e511dfd70c305
budopod1 <51675874+budopod1@users.noreply.github.com>
2025-05-01 10:54:53 -0700

    Added: test_day_end_food_consumed

--- Tests/FoodUsageTest.gd
+++ Tests/FoodUsageTest.gd
@@ -4,6 +4,21 @@ extends GutTest
 const GAME_MANAGER = preload("res://Scenes/managers/game_manager.tscn")
 
 
+func test_day_end_food_consumed() -> void:
+	ProgressManager.load_default_progress()
+	ProgressManager.stock = []
+	for i in range(len(3)):
+		var item = ProgressManager.StoredFoodComponent.new(Menu.FoodComponent.Bun, 3-i)
+		ProgressManager.stock.append(item)
+	var game_manager: GameManager = GAME_MANAGER.instantiate()
+	assert_true(game_manager.try_use_food(Menu.FoodComponent.Bun))
+	assert_true(game_manager.try_use_food(Menu.FoodComponent.Bun))
+	game_manager.update_progress_from_day()
+	assert_equal(ProgressManager.day, 2)
+	assert_eq(len(ProgressManager.stock), 1)
+	assert_eq(ProgressManager.stock[0].age, 1)
+
+
 func test_consumption() -> void:
 	ProgressManager.load_default_progress()
 	var game_manager: GameManager = GAME_MANAGER.instantiate()
