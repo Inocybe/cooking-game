f00d68237840018e24202bbe090b9b0cd12074fe
budopod1 <martin.e.staab@gmail.com>
2025-05-01 13:02:54 -0700

    Fixed: tests

--- Tests/FoodUsageTest.gd
+++ Tests/FoodUsageTest.gd
@@ -7,16 +7,16 @@ const GAME_MANAGER = preload("res://Scenes/managers/game_manager.tscn")
 func test_day_end_food_consumed() -> void:
 	ProgressManager.load_default_progress()
 	ProgressManager.stock = []
-	for i in range(len(3)):
-		var item = ProgressManager.StoredFoodComponent.new(Menu.FoodComponent.Bun, 3-i)
+	for i in range(3):
+		var item = ProgressManager.StoredFoodComponent.new(Menu.FoodComponent.Bun, 2-i)
 		ProgressManager.stock.append(item)
 	var game_manager: GameManager = GAME_MANAGER.instantiate()
 	assert_true(game_manager.try_use_food(Menu.FoodComponent.Bun))
 	assert_true(game_manager.try_use_food(Menu.FoodComponent.Bun))
 	game_manager.update_progress_from_day()
-	assert_equal(ProgressManager.day, 2)
+	assert_eq(ProgressManager.day, 2)
 	assert_eq(len(ProgressManager.stock), 1)
-	assert_eq(ProgressManager.stock[0].age, 1)
+	assert_eq(ProgressManager.stock[0].age, 1) # the age would have been 0, but one day has passed
 
 
 func test_consumption() -> void:
