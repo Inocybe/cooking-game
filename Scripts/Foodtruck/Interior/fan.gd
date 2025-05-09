f0efaaa4f4f77b3353a5eff62c36ed9edfd47f79
budopod1 <martin.e.staab@gmail.com>
2025-05-09 14:10:11 -0700

    Fixed: the fan

--- Scripts/Foodtruck/Interior/fan.gd
+++ Scripts/Foodtruck/Interior/fan.gd
@@ -1,8 +1,8 @@
 extends AnimatableBody3D
 
-@onready var fan: Node3D = $fan
 
-@export var fan_speed: float = 10
+const fan_speed: float = 10
 
-func _process(delta: float) -> void:
-	fan.rotate_y(fan_speed * delta)
+
+func _physics_process(delta: float) -> void:
+	rotate_y(fan_speed * delta)
