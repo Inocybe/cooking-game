779297739751d3dca5cf138416b127ad3cd3b07e
Inocybe <140019717+Inocybe@users.noreply.github.com>
2025-05-09 11:41:25 -0700

    food stock on tv

--- Scripts/UI/Map/BuyMenuControl.gd
+++ Scripts/UI/Map/BuyMenuControl.gd
@@ -1,17 +1,29 @@
 extends Control
 
-@onready var food_stock: TextureRect = $MarginContainer/HBoxContainer/FoodStock
+@export var food_component_chart: Dictionary[Menu.FoodComponent, Control]
 
-var stock_children: Array[Node]
+func _process(delta: float) -> void:
+	food_component_chart[0] = $VBoxContainer/FoodStockChart
+	food_component_chart[1] = $VBoxContainer/FoodStockChart2
+	food_component_chart[2] = $VBoxContainer/FoodStockChart3
+	food_component_chart[3] = $VBoxContainer/FoodStockChart4
+	
+	update_stock()
 
-func _ready() -> void:
-	stock_children = food_stock.get_children()
-	update_stock_children()
+
+func update_stock() -> void:
+	for component in Menu.FoodComponent.values():
+		update_stock_children(
+			food_component_chart[component], component
+		)
 
 
-func update_stock_children() -> void:
-	for child:Node in stock_children:
-		if child.has_method("set_food_label"):
-			child.set_food_label("")
-		if child.has_method("set_chart_length_and_text"):
-			child.set_chart_length_and_text(2)
+func update_stock_children(control: Control, component: Menu.FoodComponent) -> void:
+	if control:
+		if control.has_method("set_food_label"):
+			control.set_food_label(Menu.get_food_component_name(component))
+		if control.has_method("set_chart_length_and_text"):
+			control.set_chart_length_and_text(
+				ProgressManager.get_food_available_count(component)
+					- Global.game_manager.food_components_used.get(component, 0)
+			)
